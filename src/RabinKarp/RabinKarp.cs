57ecb001520abeca9fcece0b4c19737d2e21be2b
ferizoozoo <farhadzohoorz@gmail.com>
2021-08-09 02:45:45 +0430

    Write the Rabin-Karp algorithm (without the hash function) for searching patterns inside texts

---
+++ src/RabinKarp/RabinKarp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RabinKarp
+{
+    public class RKP
+    {
+        // If algorithm returns -1, it means that the pattern is not found in the text;
+        // Otherwise, the found index is returned.
+        public int RabinKarpSearch(string text, string pattern)
+        {
+            var hashedPattern = RollingHash(pattern);
+            for (int index = 0; index < text.Length - pattern.Length + 1; index++)
+            {
+                var partOfText = text.Substring(index, pattern.Length);
+                var hashedPartOfText = RollingHash(partOfText);
+                if (hashedPartOfText == hashedPattern)
+                    if (partOfText == pattern)
+                        return index;
+            }
+            return -1;
+        }
+
+        public string RollingHash(string text)
+        {
+
+        }
+    }
+}
