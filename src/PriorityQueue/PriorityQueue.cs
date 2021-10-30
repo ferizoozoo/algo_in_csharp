f959604ed2cf1afa4a61e384c4f32af8c7278689
ferizoozoo <farhadzohoorz@gmail.com>
2021-10-30 12:03:55 +0330

    Add Priority Queue data structure

---
+++ src/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,21 @@
+﻿using BinaryMaxHeap;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue
+    {
+        private Heap _heap;
+
+        public bool IsEmpty => _heap.Length == 0;
+
+        public void InsertWithPriority(Node node)
+        {
+            _heap.Insert(node);
+        }
+
+        public Node PullHighestPriorityElement()
+        {
+            return _heap.Extract();
+        }
+    }
+}
